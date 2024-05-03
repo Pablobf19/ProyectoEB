fc0a3a299e60832d0dd930492859ed8061cd392a
dennisgarro <sistemasitm6099@gmail.com>
2024-05-02 19:16:18 -0500

    clase y metodo empleado

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,5 +13,16 @@ public class Main {
         System.out.println(ie.Ingresaredad());
 
 
+        LinkedList<Empleado> empleado = new LinkedList<>();
+        int cant = Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese la cantidad de empleados "));
+        for (int i = 0; i < cant; i++) {
+            Empleado  e = new Empleado(null, null, null, i, null);
+            e.setNombre(JOptionPane.showInputDialog("Ingrese nombre"));
+            e.setApellido(JOptionPane.showInputDialog("Ingrese el apellido"));
+            e.setDireccion(JOptionPane.showInputDialog("Ingrese la direccion"));
+            e.setEdad(Integer.parseInt(JOptionPane.showInputDialog("Ingrese la edad")));
+            e.setCargo("Ingrese el cargo");
+            empleado.add(e);
+        }
     }
 }
